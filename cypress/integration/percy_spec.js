7e5b251d4705029336d1cf97f4a72c3a116c0a5f
Tomas <tomastoman@gmail.com>
2019-06-26 11:15:52 +0200

    remove contains react

--- cypress/integration/percy_spec.js
+++ cypress/integration/percy_spec.js
@@ -3,8 +3,6 @@ describe('Integration test with visual testing', function() {
     // Load the page or perform any other interactions with the app.
     cy.visit('http://localhost:3000');
 
-    cy.contains('React')
-
     // Take a snapshot for visual diffing
     cy.percySnapshot();
   });
