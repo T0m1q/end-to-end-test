e2deb7a9ee46b5bf649f138d41419138ca932f6a
Tomas <tomastoman@gmail.com>
2019-07-01 14:34:57 +0100

    remove cypress failing test

--- cypress/integration/percy_spec.js
+++ cypress/integration/percy_spec.js
@@ -3,8 +3,6 @@ describe('Integration test with visual testing', function() {
     // Load the page or perform any other interactions with the app.
     cy.visit('http://localhost:3000');
 
-    expect(true).to.equal(false)
-
     // Take a snapshot for visual diffing
     cy.percySnapshot();
   });
